ac7b77734e308d80813354d4b149381332161f47
adelao747 <113153195+adelao747@users.noreply.github.com>
2023-04-08 16:03:54 -0700

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify, render_template
-app=Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('firemap.html')
-if __name__=='__main__':
-    app.run()
